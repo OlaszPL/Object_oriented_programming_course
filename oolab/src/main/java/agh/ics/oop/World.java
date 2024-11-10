0a61d3b290b8cb3adfa1f4910a83b3d8b898fbb2
Aleksander Jóźwik <aleksanderjx@gmail.com>
2024-11-11 00:16:07 +0100

    Podpunkt 4 gotowy

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -9,17 +9,6 @@ import java.util.List;
 
 public class World {
 
-    public static void run(MoveDirection[] moves){
-        for (MoveDirection move : moves) {
-            switch(move){
-                case FORWARD -> System.out.println("Zwierzak idzie do przodu");
-                case BACKWARD -> System.out.println("Zwierzak idzie do tyłu");
-                case RIGHT -> System.out.println("Zwierzak idzie w prawo");
-                case LEFT -> System.out.println("Zwierzak idzie w lewo");
-            }
-        }
-    }
-
     public static void main(String[] args) {
         List<MoveDirection> directions = OptionsParser.parse(args);
         List<Animal> animals = List.of(new Animal(new Vector2d(2,2)), new Animal(new Vector2d(3,4)));
---
+++ oolab/src/main/java/agh/ics/oop/model/TextMap.java
@@ -0,0 +1,55 @@
+package agh.ics.oop.model;
+
+import java.util.ArrayList;
+
+public class TextMap implements WorldMap<String, Integer> {
+    private final ArrayList<String> list;
+
+    public TextMap() {
+        this.list = new ArrayList<>();
+    }
+
+    @Override
+    public boolean isOccupied(Integer position) {
+        return 0 <= position && position < list.size();
+    }
+
+    @Override
+    public String objectAt(Integer position) {
+        if (isOccupied(position)) return list.get(position);
+        return null;
+    }
+
+    @Override
+    public boolean canMoveTo(Integer position) {
+        return isOccupied(position);
+    }
+
+    @Override
+    public boolean place(String object) {
+        list.add(object);
+        return true;
+    }
+
+    @Override
+    public void move(String object, MoveDirection direction) {
+        int i = list.indexOf(object);
+        if (i < 0) return;
+
+        int newPos = i + switch(direction){
+            case FORWARD, RIGHT -> 1;
+            case BACKWARD, LEFT -> -1;
+        };
+
+        if (canMoveTo(newPos)){
+            String toSwap = list.get(newPos);
+            list.set(newPos, object);
+            list.set(i, toSwap);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return list.toString();
+    }
+}
